222dbccc82c42056f6305dd0a4a21f27ca2520e9
crispg72 <crispg72@users.noreply.github.com>
2017-11-21 06:47:57 +0000

    sets up stack

--- boot/src/x86_64/boot.asm
+++ boot/src/x86_64/boot.asm
@@ -3,9 +3,15 @@ global start
 section .text
 bits 32
 start:
+	mov esp, stack_top
+
     ; print `crispOS` to screen
     mov dword [0xb8000], 0x2f722f63
     mov dword [0xb8004], 0x2f732f69
     mov dword [0xb8008], 0x2f4f2f70
     mov dword [0xb800c], 0x2f53
     hlt
+
+section .bss
+	resb 64
+stack_top:
