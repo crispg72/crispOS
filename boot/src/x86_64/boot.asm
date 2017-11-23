d89ba75ac807cce3887e7c35c363649bc7a80ebe
crispg72 <crispg72@users.noreply.github.com>
2017-11-23 06:42:22 +0000

    Added output_text function for errors etc

--- boot/src/x86_64/boot.asm
+++ boot/src/x86_64/boot.asm
@@ -5,13 +5,33 @@ bits 32
 start:
 	mov esp, stack_top
 
+	mov ch, 0x2f
+	mov ebx, crispOS_str
+
     ; print `crispOS` to screen
-    mov dword [0xb8000], 0x2f722f63
-    mov dword [0xb8004], 0x2f732f69
-    mov dword [0xb8008], 0x2f4f2f70
-    mov dword [0xb800c], 0x2f53
+	call output_text
+
     hlt
+
+crispOS_str db 'crispOS', 0
+
+output_text:
+	mov edx, 0xb8000
+ot_loop:
+	mov cl, [ebx]
+
+	or cl, cl
+	je ot_done
+
+	mov [edx], cx
+
+	adc edx, 2
+	inc ebx
+	jmp ot_loop
+ot_done:
+	ret
 
 section .bss
 	resb 64
 stack_top:
+	
