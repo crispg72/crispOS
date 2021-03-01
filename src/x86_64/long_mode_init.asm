01e6dd20522bfda9484803fafbb4ca4ef7270c4b
crispg72 <chris.e.green@hotmail.com>
2021-03-01 19:18:05 +0000

    x86_64 calling convention

--- src/x86_64/long_mode_init.asm
+++ src/x86_64/long_mode_init.asm
@@ -1,14 +1,18 @@
 global long_mode_init
+global output_text64
+
+extern kernel_init
 
 section .text
 bits 64
 long_mode_init:
 
 	mov ch, 0x2f
-	mov rbx, crispOS_str64
+	mov rdi, crispOS_str64
 
     ; print `crispOS` to screen
-	call output_text64
+	;call output_text64
+	call kernel_init
 
     hlt
 
@@ -17,7 +21,7 @@ crispOS_str64 db 'crispOS', 0
 output_text64:
 	mov rdx, 0xb8000
 .ot_loop:
-	mov cl, [rbx]
+	mov cl, [rdi]
 
 	or cl, cl
 	je .ot_done
@@ -25,7 +29,7 @@ output_text64:
 	mov [rdx], cx
 
 	adc rdx, 2
-	inc rbx
+	inc rdi
 	jmp .ot_loop
 .ot_done:
 	ret
